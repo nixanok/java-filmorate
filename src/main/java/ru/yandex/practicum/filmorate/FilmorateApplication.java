0416fc9f690c3218f64fe42e83318a12f24a03da
nixanok <mrfreemen555@gmail.com>
2023-08-12 16:18:43 +0300

    <opt>:"Код приведен к чекстайлу."

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,11 +1,12 @@
 package ru.yandex.practicum.filmorate;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class FilmorateApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
