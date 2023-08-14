06e0e2163e548af923f1ce2c1b1d6b8e06baff3c
nixanok <mrfreemen555@gmail.com>
2023-08-15 02:18:48 +0300

    <opt>:"Код приведен к чекстайлу."

--- src/main/java/ru/yandex/practicum/filmorate/exception/FriendsAlreadyExistException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FriendsAlreadyExistException.java
@@ -2,6 +2,6 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class FriendsAlreadyExistException extends RuntimeException {
     public FriendsAlreadyExistException(int firstId, int secondId) {
-        super (String.format("Friends with id = %s and id = %s already exist.", firstId, secondId));
+        super(String.format("Friends with id = %s and id = %s already exist.", firstId, secondId));
     }
 }
