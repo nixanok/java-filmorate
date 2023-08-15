9c69c4e811e5075adf0e41cae76b2a3c75195417
nixanok <mrfreemen555@gmail.com>
2023-08-15 11:07:08 +0300

    <opt>:"Код приведен к чекстайлу."

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -70,12 +70,6 @@ CREATE TABLE IF NOT EXISTS PUBLIC.friends (
 );
 CREATE INDEX IF NOT EXISTS pk_friends_index ON PUBLIC.friends (first_id, second_id);
 
-INSERT INTO mpa (mpa_id, name)
-VALUES (1, 'G'), (2, 'PG'), (3, 'PG-13'), (4, 'R'), (5, 'NC-17');
-
-INSERT INTO genre (genre_id, name)
-VALUES (1, 'Комедия'), (2, 'Драма'), (3, 'Мультфильм'), (4, 'Триллер'), (5, 'Документальный'), (6, 'Боевик');
-
 
 
 
