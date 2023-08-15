5cea1e75631c0dccd38c64c6459a62ce52a6ca08
nixanok <mrfreemen555@gmail.com>
2023-08-15 11:01:44 +0300

    <opt>:"Код приведен к чекстайлу."

--- src/main/resources/sql/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO mpa (mpa_id, name)
-VALUES (1, 'G'), (2, 'PG'), (3, 'PG-13'), (4, 'R'), (5, 'NC-17');
-
-INSERT INTO genre (genre_id, name)
-VALUES (1, 'Комедия'), (2, 'Драма'), (3, 'Мультфильм'), (4, 'Триллер'), (5, 'Документальный'), (6, 'Боевик');
